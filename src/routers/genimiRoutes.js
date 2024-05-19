29d34687537b0750c1b07e7062d3e62611dd7f60
AbhinavKumarMaple <abhinavmaple@gmail.com>
2024-05-20 00:00:50 +0530

    Update genimiRoutes.js

--- src/routers/genimiRoutes.js
+++ src/routers/genimiRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/prompt", handleGeminiRequest);
 router.get("/test", (req, res) => {
-  res.send("Hello World!");
+  handleGeminiRequest(req, res);
 });
 
 module.exports = router;
