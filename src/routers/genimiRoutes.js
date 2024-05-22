a29d70c3c4b7bd82dd4ee86216eb73801fb3a99b
hirdeshmewada <hddj221@gmail.com>
2024-05-22 10:48:34 +0530

    feat:added get all products

--- src/routers/genimiRoutes.js
+++ src/routers/genimiRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
 const { handleGeminiRequest, updateInventories } = require("../controllers/geminiControllers");
+const { getAllProducts } = require("../services/getAllProducts");
 
 const router = express.Router();
 
 router.post("/", handleGeminiRequest);
 router.post("/realtimeupdate", updateInventories);
+router.get("/allproducts", getAllProducts)
+
 
 module.exports = router;
